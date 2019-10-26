183e6a5c4153eddbdf81f9fc635a52c7b75ec048
Frank <b439988l@gmail.com>
2019-10-26 16:10:36 +1000

    minor changes

--- src/Employer.js
+++ src/Employer.js
@@ -7,6 +7,7 @@ export function Employer() {
     const [music, setMusic] = useState(50);
     const [stress, setStress] = useState(50);
     const [cleanliness, setCleanliness] = useState(50);
+    const [total, setTotal] = useState(0);
 
     useEffect(() => {
         fetch('http://localhost:3001/surveys/music', { method: 'GET' })
@@ -41,16 +42,27 @@ export function Employer() {
             .catch((err) => {
                 console.log(err.toString())
             })
+        
+            fetch('http://localhost:3001/surveys/total', { method: 'GET' })
+            .then((res) => {
+                return res.json()
+            })
+            .then((res) => {
+                setTotal(res.count)
+            })
+            .catch((err) => {
+                console.log(err.toString())
+            })
     })
 
     return (
         <div>
             <Nav />
-            <h2>Employee Insight For Today</h2>
+            <h2>Insight From {total} Employees</h2>
             <Gauge value={music} label="Music" color="#359CBF"/>
         <Gauge value={stress} label="Stress" color="#FE9400"/>
         <Gauge value={cleanliness} label="Cleanliness" color="#92C731"/>
-        <h2>Employee Insight For This Month</h2>
+        <h2>Employee Insight From This Month</h2>
             <Gauge value={56.2} label="Music" color="#3182D6"/>
         <Gauge value={88.5} label="Stress" color="#E8A20C"/>
         <Gauge value={44.7} label="Cleanliness" color="#DEDE2C"/>
